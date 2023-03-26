c959d9e30436fd122dc447a754228bcaa5554024
Lasse <lasse@letager.dk>
2023-03-26 23:05:17 +0200

    Template matching

--- PlanarGraph/planar.py
+++ PlanarGraph/planar.py
@@ -47,6 +47,13 @@ def random_graph(V_size, E_chance = 1):
     edges = set()
 
     for i in range(V_size):
+        # Have 2 edges out of each vertex
+        for k in range(2):
+            p = random.randint(0,V_size-1)
+            while p == i:
+                p = random.randint(0,V_size-1)
+            edges.add((i,p))
+
         for j in range(i+1, V_size):
             if random.randint(0,E_chance) == 0:
                 edges.add((i,j))
@@ -68,7 +75,7 @@ def print_graph(vertices, edges):
         x1, y1, c1 = vertices[j]
         print_line(x0,y0,x1,y1,c0)
 
-vertices, edges = random_graph(10, 2)
+vertices, edges = random_graph(10, 100)
 print_graph(vertices, edges)
 
 print ("Random map")
@@ -83,14 +90,14 @@ img.save('random.png')
 
 def st_numbering(vertices, edges, s, t):
     # mark s, t and {s,t} old, all other vertices and edges new
-    
+
     edge_map = {v: list() for v in range(len(vertices))} # list should be set
     # edge_map[t].append(s) # make sure dfs visits s first
     for u,v in edges:
         edge_map[u].append(v)
         edge_map[v].append(u)
 
-    print ("EM:", edge_map)
+    # print ("EM:", edge_map)
 
     old_edges = set()
     old_vertices = set()
@@ -102,7 +109,7 @@ def st_numbering(vertices, edges, s, t):
     dfs_number = [0 for v in range(len(vertices))]
 
     parrent = [0 for v in range(len(vertices))]
-    
+
     pre = {v: 0 for v in range(len(vertices))}
     current = 1
     pre[s] = current
@@ -130,13 +137,13 @@ def st_numbering(vertices, edges, s, t):
         return low
     dfs(s) # Get dfs spanning tree.
 
-    print (s,t)
-    print ("DFS", dfs_number)
-    print (tree_edge)
-    print (back_edge)
-    print (low_node)
-    print (parrent)
-    print (preorder)
+    # print ("S<T",s,t)
+    # print ("DFS", dfs_number)
+    # print (tree_edge)
+    # print (back_edge)
+    # print (low_node)
+    # print (parrent)
+    # print (preorder)
 
     L = [s,t]
 
@@ -150,15 +157,15 @@ def st_numbering(vertices, edges, s, t):
                 pvi = i
                 break
         if sign[low_node[v]] == -1:
-            print ("before (before",parrent[v], v,")", L)
+            # print ("before (before",parrent[v], v,")", L)
             L.insert(pvi, v)
-            print ("after (before)", L)
+            # print ("after (before)", L)
             sign[parrent[v]] = 1
         else: # sign(low(v)) == 1
             L.insert(pvi+1, v)
             sign[parrent[v]] = -1
-    print (sign)
-    print (L)
+    # print (sign)
+    # print (L)
 
     return {v: i for i,v in enumerate(L)}
 
@@ -213,12 +220,132 @@ def st_numbering(vertices, edges, s, t):
 
 # print (nums)
 
-def planar ():
+def virtual_edges (edges, st_nums):
+    ret_edges = set()
+    for a,b in edges:
+        ret_edges.add((st_nums[a], st_nums[b]))
+    return ret_edges
 
+def Gk (edges, k):
+    Gk_edge = set()
+    cross_edge = set()
+    for a, b in edges:
+        if a < k:
+            if b < k:
+                Gk_edge.add((a,b))
+            else:
+                cross_edge.add((a,b))
+        else:
+            if b < k:
+                cross_edge.add((a,b)) # Reverse direction
 
+    return Gk_edge, cross_edge
+
+def upward_embed(edges):
+    pass
+
+# template matchings
+
+def not_pertient():
+    return True
+
+# Classifications, is_full, is_empty
+def classify_children(P):
+    return []
+    
+def template_matching(node):
+    cassification = classify_children(P)
+    is_full = len(filter(lambda x: x[0], cassification)) == len(classification)
+    is_empty = len(filter(lambda x: not x[0], cassification)) == len(classification)
+
+    if node[0] == "P":
+        # Template P0
+        if len(filter(lambda x: not x[0], cassification)) == len(classification): # is_empty
+            return classification, "empty"
+        # Template P1
+        if len(filter(lambda x: not x[0], cassification)) == len(classification): # is_full
+            return classification, "full"
+        # Template P2 (root)
+        swap_index = 0
+        for i in cassification:
+            if not i[0] == "empty":
+                swap_index = i
+                break
+        for i in cassification[swap_index+1:]:
+            if not i[0] == "full":
+                break
+        else: # Matches
+            return "P",classification[:swap_index] + [("P",classification,"full")], "root?"
+        # Template P3 (not root)
+        swap_index = 0
+        for i in cassification:
+            if not i[0] == "empty":
+                swap_index = i
+                break
+        for i in cassification[swap_index+1:]:
+            if not i[0] == "full":
+                break
+        else: # Matches
+            return "P",classification[:swap_index] + [("P",classification,"full")], "singly partial"
+        # Template P4 (root)
+        swap_index = 0
+        p4_index = list(filter(lambda x: x[0] == "partial", enumerate(cassification)))
+        if len(p4_index) == 1:
+            index, val = p4_index[0]
+            return "P",classification[:index] + classification[index+1:] + classification[index], "root?"
+        # Template P5
+        # Template P6
+    elif node[0] == "Q":
+        # Template Q0 (Like P0)
+        if len(filter(lambda x: not x[0], cassification)) == len(classification): # is_empty
+            return node, "empty"
+        # Template Q1 (Like P1)
+        if len(filter(lambda x: not x[0], cassification)) == len(classification): # is_full
+            return node, "full"
+
+        # Template Q2
+        # Template Q3
+    elif node[0] == "leaf":
+    else:
+        print ("Formatting error")
+
+    # if is_root:
+    #     if is_full:
+    #         # P nodes (if all empty), identity replacement and empty node
+    #         return ("empty", P)
+    #     elif is_empty:
+    #         # P nodes (if all full), identity replacement and full node
+    #         return ("full", P)
+    #     else:
+    #         # P nodes (partial), make new full P node, and collect all full children under that
+    #         return ("full",)
+    # else:
+
+    return True
+
+def planar (vertices, edges):
+    s, t = next(iter(edges))
+    nums = st_numbering(list(range(len(vertices))), edges, s, t) # 0 = s, 9 = t, 1..8 = a..h
+    print (nums)
+
+    v_edges = virtual_edges(edges, nums)
+    v_verts = list(range(len(vertices)))
+
+    print (Gk(edges, 1))
+
+    for v in range(2,len(vertices)):
+        while not_pertient():
+            if not template_matching():
+                print ("Not planar")
+                return False
+        # Replace full node of PQ tree by a P-node
+        # Add all neighbord of v larger than v as the sons of the P-node
+    print ("G is planar")
+    return True
+
+planar(vertices, edges)
 
 # st_numbering(vertices, edges)
 
 # def planar(vertices, edges):
 #     for v in range(2, n):
-
